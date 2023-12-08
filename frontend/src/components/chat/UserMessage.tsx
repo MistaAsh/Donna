6b21b7f2647b324a21bba99fe5c5bc215a86988c
Malavika Harikumar <79362360+richard1615@users.noreply.github.com>
2023-12-08 21:32:56 +0000

    Styled user and bot messages

--- frontend/src/components/chat/UserMessage.tsx
+++ frontend/src/components/chat/UserMessage.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const UserMessage = ({message}) => {
+const UserMessage = ({ message }) => {
   return (
-      <p className='text-end p-3 bg-gray-300 rounded-l-[15px] rounded-tr-[15px] text-black flex justify-start mt-3 max-w-3xl self-end'>
-        {message}
-      </p>
-  )
-}
+    <div className='flex justify-end w-full relative mb-6'>
+      <div className='flex bg-slate-300 rounded-2xl p-5 max-w-2xl overflow-hidden'>
+        <div className='flex flex-col text-white flex-grow'>
+          {message}
+        </div>
+      </div>
+      <span className='w-10 h-10 rounded-bl-full bg-slate-300 absolute bottom-0 right-0 transform translate-y-1/2'></span>
+    </div>
+  );
+};
 
 export default UserMessage;
