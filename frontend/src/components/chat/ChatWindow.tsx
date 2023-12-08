728dba38e9f5ff63d15ecb7d6ef7577b0f1fcbeb
Malavika Harikumar <79362360+richard1615@users.noreply.github.com>
2023-12-08 20:06:03 +0000

    Integrated real time supabase updates

--- frontend/src/components/chat/ChatWindow.tsx
+++ frontend/src/components/chat/ChatWindow.tsx
@@ -1,40 +1,78 @@
-import React from 'react'
-import UserMessage from './UserMessage'
-import BotMessage from './BotMessage'
-import ChatBox from './ChatBox'
-import Header from './Header'
+import React, { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import UserMessage from './UserMessage';
+import BotMessage from './BotMessage';
+import ChatBox from './ChatBox';
+import Header from './Header';
 
 const ChatWindow = () => {
-    const messages = [
-        {
-            content: 'something',
-            type: 'bot'
-        },
-        {
-            content: 'something',
-            type: 'bot'
-        },
-        {
-            content: 'something 2',
-            type: 'user'
-        }
-    ]
+  const supabase = createClient(
+    'https://ydlodplhsscvfhxfgiha.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkbG9kcGxoc3NjdmZoeGZnaWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwNjM3MjIsImV4cCI6MjAxNzYzOTcyMn0.Y41Q9wTAHmPf9sH4DAUL56Z_O1RneJmH_aZPHGH_-DY'
+  );
+  const [messages, setMessages] = useState([]);
 
-    return (
-        <div className='flex flex-col justify-between bg-black h-screen mx-[300px]'>
-            <div className='overflow-y-auto flex flex-col gap-3.5'>
-                <Header />
-                <div className='flex flex-col flex-grow p-5 overflow-y-scroll'>
-                    {messages.map(({ type, content }) => (
-                        type === 'bot' ?
-                            <BotMessage message={content} />
-                            : <UserMessage message={content} />
-                    ))}
-                </div>
-            </div>
-            <ChatBox />
+  useEffect(() => {
+    async function getMessages() {
+      try {
+        const { data, error } = await supabase
+          .from('messages')
+          .select('content, type')
+          .eq('session_id', 1)
+          .order('created_at', { ascending: false });
+        if (data === null) {
+          setMessages([]);
+        } else {
+          setMessages(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMessages();
+  }, []);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('realtime posts')
+      .on('postgres_changes', {
+        event: 'INSERT',
+        schema: 'public',
+        table: 'messages',
+      }, payload => {
+        setMessages(prevMessages => {
+          // Check if message with this ID already exists and the session ID matches
+          if (prevMessages.some(msg => msg.id === payload.new.id) && payload.new.session_id === 1) {
+            return prevMessages; // Return the unchanged state
+          }
+          // Otherwise, append the new message
+          return [payload.new, ...prevMessages];
+        });
+      }).subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    }
+  }, [supabase]);
+
+
+  return (
+    <div className='flex flex-col justify-between bg-black h-screen mx-[300px]'>
+      <div className='overflow-y-auto flex flex-col gap-3.5'>
+        <Header />
+        <div className='flex flex-col-reverse flex-grow p-5 overflow-y-scroll'>
+          {messages.map(({ type, content }) =>
+            type === 'bot' ? (
+              <BotMessage message={content} />
+            ) : (
+              <UserMessage message={content} />
+            )
+          )}
         </div>
-    )
-}
+      </div>
+      <ChatBox />
+    </div>
+  );
+};
 
 export default ChatWindow;
