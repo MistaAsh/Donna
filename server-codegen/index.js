6c95118fa01cbc43b4c24f793d065b158942d936
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-12-10 04:04:43 +0530

    thirdweb contract deployment

--- server-codegen/index.js
+++ server-codegen/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const ethers = require('ethers');
 const Web3 = require('web3');
+const { ThirdwebSDK, directDeployDeterministic } =  require("@thirdweb-dev/sdk");
 
 // Function to find and read OpenZeppelin contracts
 const findImports = (importPath) => {
@@ -61,8 +62,14 @@ if (output.errors) {
 }
 
 
-const privateKey = "7fb70f345bcc41dbfa5887bfe5e515c2bcfbf901464c2b8b744c2a50b75a22cf";
-const signer = new ethers.Wallet(privateKey);
+// const privateKey = "7fb70f345bcc41dbfa5887bfe5e515c2bcfbf901464c2b8b744c2a50b75a22cf";
+// const signer = new ethers.Wallet(privateKey);
+
+
+const sdk = ThirdwebSDK.fromPrivateKey(`7fb70f345bcc41dbfa5887bfe5e515c2bcfbf901464c2b8b744c2a50b75a22cf`, "goerli", {
+    clientId: "887a8d5f58c954d65c38cbef774cd1a0", // Use client id if using on the client side, get it from dashboard settings
+  });
+
 
 async function deployContract(abi, bytecode) {
     // Connect to Ethereum - replace with your Ethereum node URL
@@ -72,26 +79,34 @@ async function deployContract(abi, bytecode) {
     const account = '0x39BeC0c9001C72EeaA82BcD66773fB5eb62d39D7';
     const privateKey = '7fb70f345bcc41dbfa5887bfe5e515c2bcfbf901464c2b8b744c2a50b75a22cf';
 
-    const MyContract = new web3.eth.Contract(abi);
+    const signer = await sdk.getSigner();
+    // const MyContract = new web3.eth.Contract(abi);
 
-    const deploy = MyContract.deploy({
-        data: '0x' + bytecode,
-        arguments: [] // Any constructor arguments go here
-    });
+    // const deploy = MyContract.deploy({
+    //     data: '0x' + bytecode,
+    //     arguments: [] // Any constructor arguments go here
+    // });
 
-    const gas = 2000000;
-    const gasPrice = await web3.eth.getGasPrice();
+    // const gas = 2000000;
+    // const gasPrice = await web3.eth.getGasPrice();
 
-    const signedTransaction = await web3.eth.accounts.signTransaction({
-        from: account,
-        data: deploy.encodeABI(),
-        gas,
-        gasPrice
-    }, privateKey);
+    // const signedTransaction = await web3.eth.accounts.signTransaction({
+    //     from: account,
+    //     data: deploy.encodeABI(),
+    //     gas,
+    //     gasPrice
+    // }, privateKey);
 
-    const receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+    // const receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
 
-    console.log('Contract deployed at address:', receipt.contractAddress);
+    const deployedContractAddress = await directDeployDeterministic(
+        bytecode,
+        abi,
+        signer,
+        [],
+        "",
+      );
+    console.log('Contract deployed at address:', deployedContractAddress);
 }
 
 // Assuming contract compilation was successful
