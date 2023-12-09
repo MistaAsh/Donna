a89856c31ea767c42ea460823e8289e4810411bb
Malavika Harikumar <79362360+richard1615@users.noreply.github.com>
2023-12-09 21:44:41 +0000

    Transactions

--- frontend/src/components/transactionHistory.tsx
+++ frontend/src/components/transactionHistory.tsx
@@ -45,9 +45,33 @@ const TransactionHistoryComponent = ({ chainId, address }) => {
     <div>
       {transactions.map((transaction, index) => (
         <div key={index}>
-          {/* Render your transaction details here */}
-          <p>Transaction Hash: {transaction.tx_hash}</p>
-          {/* Add more transaction details as needed */}
+          <div key={transaction.date} className='flex border-b-slate-200 border-b-[1px]'>
+            <div className="items-stretch flex gap-4 pr-4 py-4">
+              <div className="items-center flex justify-between gap-4 pr-16 max-md:pr-5">
+                <div className="items-stretch self-stretch flex grow basis-[0%] flex-col">
+                  <div className="text-neutral-700 text-base font-bold leading-6 whitespace-nowrap">
+                    <span className="text-neutral-700">{transaction.type}</span>
+                    <span className="text-neutral-700">&nbsp;{transaction.amount}&nbsp;{transaction.currency}</span>
+                  </div>
+                  <div className="items-center flex justify-between gap-4">
+                    <div className="items-stretch flex gap-1.5 my-auto">
+                      <div className="text-neutral-700 text-opacity-60 text-sm font-medium tracking-normal grow whitespace-nowrap">
+                        {transaction.address.substring(0, 2)}...{transaction.address.slice(-2)}
+                      </div>
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/73fb33e673669b7868f52e6633b49936e329dfb5e66a28dd7f6c8f1b885e6739?apiKey=3d055ebf140b4df39c45c33773b9d984&"
+                        className="aspect-square object-contain object-center w-3.5 overflow-hidden shrink-0 max-w-full self-start"
+                      />
+                    </div>
+                    <div className="ml-2 text-neutral-700 text-opacity-60 text-sm font-medium leading-5 self-stretch grow whitespace-nowrap">
+                      11:34 PM IST
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
